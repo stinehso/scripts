06725077b9d20c1a7bae52171da35a1d965ba9a6
Stine <stinehso@gmail.com>
2019-09-06 14:38:56 +0200

    New script, does not work yet

---
+++ climb.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ "$#" == "0" ]; then
+	number=1
+elif [ "$#" == "1" ]; then
+	number=$1
+else
+	echo "using first number"
+	number=$1
+fi
+
+str="cd .."
+var="/.."
+
+i=1
+while [ $i -lt $number ]; do
+	echo $i
+	let i=$i+1
+	str="$str$var"
+done
+#echo $str
+
+$str
+
+cd ..
